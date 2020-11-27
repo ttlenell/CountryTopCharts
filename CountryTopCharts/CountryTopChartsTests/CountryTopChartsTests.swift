8923e867dd2c76eeb3cf50d091a5465b0e074a45
Tobias Claesson <tobbe.classon@hotmail.se>
2020-11-27 10:29:56 +0100

    project started

---
+++ CountryTopCharts/CountryTopChartsTests/CountryTopChartsTests.swift
@@ -0,0 +1,34 @@
+//
+//  CountryTopChartsTests.swift
+//  CountryTopChartsTests
+//
+//  Created by Tobias Classon on 2020-11-27.
+//  Copyright © 2020 Tobias Classon. All rights reserved.
+//
+
+import XCTest
+@testable import CountryTopCharts
+
+class CountryTopChartsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
