709b7dca9ef8f50c2cd6b5e4f870d2922673017b
THOMAS BERGGREN <thomaslenell1@gmail.com>
2020-12-15 14:50:58 +0100

    renamed news to sources and added NewsPresenter

---
+++ CountryTopCharts/CountryTopCharts/Presenters/NewsPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  NewsPresenter.swift
+//  CountryTopCharts
+//
+//  Created by Thomas Lenell on 2020-12-15.
+//  Copyright © 2020 Tobias Classon. All rights reserved.
+//
+
+import Foundation
